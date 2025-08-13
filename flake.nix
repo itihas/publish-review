d6652aa126969b576bfc4769006ad66a0bc56561
itihas <sahiti93@gmail.com>
2025-08-13 16:39:59 +0530

    fix refs, rename

--- flake.nix
+++ flake.nix
@@ -2,22 +2,22 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = { self, nixpkgs, ... }: {
-    flakeModules.default = {
-      options.src = with nixpkgs.lib; mkOption { type = types.path; };
+  outputs = { nixpkgs, ... }: {
+    flakeModules.default = localFlake: {
+      options.publishSrc = with nixpkgs.lib; mkOption { type = types.path; };
       config = {
         systems = [ "x86_64-linux" ];
-        perSystem = { inputs, pkgs, system, ... }: {
+        perSystem = { self', inputs, pkgs, system, ... }: {
           packages = let pkgs = nixpkgs.legacyPackages."x86_64-linux";
           in rec {
             default = push;
-            push = pkgs.writeShellScriptBin "push-review" ''
+            push = pkgs.writeShellScriptBin "push" ''
               rm -rf /tmp/review
               cd /tmp
               git clone git@github.com:itihas/review --depth=1
               cd review
               ${pkgs.rsync}/bin/rsync --verbose --recursive ${
-                self.packages."x86_64-linux".build
+                self'.packages.site
               }/ ./
               chown -R  $USER:users .
               git status
@@ -27,9 +27,9 @@
               git commit -m "flake build"
               git push
             '';
-            build = pkgs.stdenv.mkDerivation {
-              name = "itihas-review";
-              src = self.src;
+            site = pkgs.stdenv.mkDerivation {
+              name = "site";
+              src = localFlake.config.publishSrc;
               buildInputs = [
                 (pkgs.emacs-nox.pkgs.withPackages
                   (epkgs: with epkgs; [ org citar org-roam org-roam-bibtex ]))
